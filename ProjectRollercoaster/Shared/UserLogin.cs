64133d7c14e0347c7b1d5e62855b3274e0e00c29
LinusNestorson <lnestorson@gmail.com>
2021-12-29 18:32:08 +0100

    Added a loginpage and some zones for future articles on index page

---
+++ ProjectRollercoaster/Shared/UserLogin.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectRollercoaster.Shared
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please enter a username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password")]
+        public string Password { get; set; }
+    }
+}
